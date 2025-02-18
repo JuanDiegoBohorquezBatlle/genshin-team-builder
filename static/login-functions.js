c079190d7f709e7cee4721f67c9a87b1d1809327
syeet06 <suiyee06@gmail.com>
2025-02-19 01:34:31 +0800

    formatting

--- static/login-functions.js
+++ static/login-functions.js
@@ -1,5 +1,5 @@
 // Define the API base URL
-const BASE_URL = 'http://localhost:8000';  
+const BASE_URL = 'https://genshinteambuilder.xyz';  
 
 // Utility function to toggle element visibility
 function toggleVisibility(elementId, show) {
